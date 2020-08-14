04c0ccb1f63bdecf86fc8dac4f5524e37c97e9a0
Taynan Souza <taynan.tws@gmail.com>
2020-08-14 12:01:56 -0300

    Favoring the teachers from the list

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, Text, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
@@ -7,6 +7,7 @@ import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export interface Teacher {
   id: number;
@@ -19,14 +20,40 @@ export interface Teacher {
 }
 
 interface TeacherItemProps {
-  teacher: Teacher
+  teacher: Teacher,
+  favorited: boolean
 }
 
-const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher, favorited}) => {
+  const [isFavorited, setIsFavorited] = useState(favorited);
+
   function handleLinkToWhatsapp() {
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
   }
 
+  async function handleToggleFavorite() {
+    const favorites = await AsyncStorage.getItem('favorites')
+    let favoritesArray = [];
+    if (favorites) {
+      favoritesArray = JSON.parse(favorites);
+    }
+
+    if (isFavorited) {
+      const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+        return teacherItem.id === teacher.id;
+      })
+
+      favoritesArray.splice(favoriteIndex, 1);
+      setIsFavorited(false);
+
+    } else {
+
+      favoritesArray.push(teacher);
+      setIsFavorited(true);
+    }
+
+    await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
+  }
 
   return (
     <View style={styles.container}>
@@ -56,10 +83,13 @@ const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
 
         <View style={styles.buttonsContainer}>
           <RectButton
-            style={[styles.favoriteButton, styles.favorited]}
+            onPress={handleToggleFavorite}
+            style={[
+              styles.favoriteButton,
+              isFavorited ? styles.favorited : {}
+            ]}
           >
-            {/*<Image source={heartOutlineIcon}/>*/}
-            <Image source={unfavoriteIcon}/>
+            {isFavorited ? <Image source={unfavoriteIcon}/> : <Image source={heartOutlineIcon}/>}
           </RectButton>
 
           <RectButton
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from "react-native";
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem, { Teacher } from '../../components/TeacherItem';
@@ -11,19 +12,36 @@ import api from '../../services/api';
 import styles from './styles';
 
 function TeacherList() {
-  const [ teachers, setTeachers] = useState([]);
+  const [teachers, setTeachers] = useState([]);
+
+  const [favorites, setFavorites] = useState<number[]>([]);
 
   const [isFiltersVisible, setIsFilterVisible] = useState(false);
   const [subject, setSubject] = useState('');
   const [week_day, setWeekDay] = useState('');
   const [time, setTime] = useState('');
 
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites')
+      .then(response => {
+        if (response) {
+          const favoritedTeachers = JSON.parse(response);
+          const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+            return teacher.id
+          })
+
+          setFavorites(favoritedTeachersIds);
+        }
+      })
+  }
 
   function handleToggleFilterVisible() {
     setIsFilterVisible(!isFiltersVisible);
   }
 
   async function handleFilterSubmit() {
+    loadFavorites();
+
     const response = await api.get('classes', {
       params: {
         subject,
@@ -96,7 +114,15 @@ function TeacherList() {
           paddingBottom: 16
         }}
       >
-        {teachers.map((teacher: Teacher) => <TeacherItem key={teacher.id} teacher={teacher}/>)}
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          )
+        })}
       </ScrollView>
     </View>
   )
